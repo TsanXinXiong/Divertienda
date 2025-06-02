7db5082df639484d5a66a4f55238a5860ec08545
Valgavoth <sanchinchong@gmail.com>
2025-06-01 20:50:20 -0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const App = () => (
+  <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+    <Text>Bienvenido a Divertienda 🍻</Text>
+  </View>
+);
+
+export default App;
