0a94dd689aafa1af5a60509dd3bf28beab70f375
Valgavoth <sanchinchong@gmail.com>
2025-06-01 20:59:07 -0500

    Add files via upload

---
+++ admin/AdminPanel.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const AdminPanel = () => (
+  <View>
+    <Text>Panel Administrativo</Text>
+  </View>
+);
+
+export default AdminPanel;
